d7204043cbb6bdf07f25a313394ab02ac7597f73
Alejandro Garrocho Cruz <101363464+AlejandroGC19@users.noreply.github.com>
2024-11-14 22:12:08 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,70 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const symbols = ["♦", "♥", "♠", "♣"];
+const cardsValues = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+
+function renderCard(symbol, value) {
+  let color = "";
+  if (symbol === "♦" || symbol === "♥") color = "text-danger";
+
+  document.querySelector(".card").innerHTML = `
+    <div class="symbol d-flex align-items-center ${color} h-25">${symbol}</div>
+    <div class="value d-flex align-items-center justify-content-center h-50">${value}</div>
+    <div class="symbol reverse d-flex align-items-center ${color} h-25">${symbol}</div>`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  newCard();
+  const card = document.getElementById("card");
+  let intervalID;
+
+  const newCardBtn = document.getElementById("newCardBtn");
+  const cardWidth = document.getElementById("cardWidth");
+  const cardHeight = document.getElementById("cardHeight");
+  const timeSelect = document.getElementById("timeSelect");
+
+  newCardBtn.addEventListener("click", newCard);
+
+  cardWidth.addEventListener("change", () => {
+    card.style.width = `${cardWidth.value}px`;
+    cardHeight.value = "";
+    cardWidth.value > 100
+      ? (card.style.fontSize = `${cardWidth.value / 2.8}px`)
+      : (card.style.fontSize = `39px`);
+  });
+  cardHeight.addEventListener("change", () => {
+    card.style.width = `${(cardHeight.value * 6) / 9}px`;
+    cardWidth.value = "";
+    cardHeight.value > 150
+      ? (card.style.fontSize = `${((cardHeight.value / 2.8) * 6) / 9}px`)
+      : (card.style.fontSize = `39px`);
+  });
+
+  timeSelect.addEventListener("change", () => {
+    clearInterval(intervalID);
+    if (timeSelect.value !== "disabled") {
+      intervalID = setInterval(newCard, parseInt(timeSelect.value * 1000));
+    }
+  });
 };
+
+function newCard() {
+  let cardSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+  let cardValue = cardsValues[Math.floor(Math.random() * cardsValues.length)];
+  renderCard(cardSymbol, cardValue);
+}
